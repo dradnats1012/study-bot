4d60bd594a46312fdb76bdb66560b4ded027cdf8
dradnats1012 <herjebi1012@gmail.com>
2025-03-10 21:15:21 +0900

    fix: User FetchType.LAZY 적용

--- src/main/java/org/example/studybot/model/User.java
+++ src/main/java/org/example/studybot/model/User.java
@@ -2,6 +2,7 @@ package org.example.studybot.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,7 +28,7 @@ public class User {
     @Column(name = "nickName", nullable = false)
     private String nickName;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "team_id", nullable = false)
     private Team team;
 }
