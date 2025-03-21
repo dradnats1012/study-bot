d86c4b5d0cfdd58a1e06735224022c800693447b
dradnats1012 <herjebi1012@gmail.com>
2025-03-21 20:34:18 +0900

    feat: channle - team 관계 재설정, 빌더

--- src/main/java/org/example/studybot/model/Channel.java
+++ src/main/java/org/example/studybot/model/Channel.java
@@ -2,20 +2,20 @@ package org.example.studybot.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "channel")
 @Getter
 @Setter
+@NoArgsConstructor
 public class Channel {
 
     @Id
@@ -28,7 +28,12 @@ public class Channel {
     @Column(name = "channel_name", nullable = false)
     private String channelName;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "team_id", nullable = false)
-    private Team team;
+    @Builder
+    public Channel(
+        String discordChannelId,
+        String channelName
+    ) {
+        this.discordChannelId = discordChannelId;
+        this.channelName = channelName;
+    }
 }
