a73d8428810a94c00b6862379b9d8a9c5379ee12
dradnats1012 <herjebi1012@gmail.com>
2025-03-21 20:34:56 +0900

    feat: 각 테이블별 repository, service 생성

---
+++ src/main/java/org/example/studybot/repository/RecordRepository.java
@@ -0,0 +1,7 @@
+package org.example.studybot.repository;
+
+import org.example.studybot.model.Record;
+import org.springframework.data.repository.Repository;
+
+public interface RecordRepository extends Repository<Record, Long> {
+}
