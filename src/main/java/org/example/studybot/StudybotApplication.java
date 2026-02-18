cc0f4cbe6ad5f26d2336525143dd4b42a793bf61
dradnats1012 <herjebi1012@gmail.com>
2026-02-18 23:26:15 +0900

    feat: 분리

--- src/main/java/org/example/studybot/StudybotApplication.java
+++ src/main/java/org/example/studybot/StudybotApplication.java
@@ -1,36 +1,14 @@
 package org.example.studybot;
 
-
-import org.example.studybot.listener.StudyBotDiscordListener;
-import org.example.studybot.util.DiscordBotToken;
-import org.example.studybot.listener.VoiceChannelTracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.requests.GatewayIntent;
 
 @SpringBootApplication
 @EnableScheduling
 public class StudybotApplication {
 
-    public static void main(String[] args){
-        ApplicationContext context = SpringApplication.run(StudybotApplication.class, args);
-        DiscordBotToken discordBotTokenEntity = context.getBean(DiscordBotToken.class);
-        String discordBotToken = discordBotTokenEntity.getDiscordBotToken();
-
-        JDA jda = JDABuilder.createDefault(discordBotToken)
-            .setActivity(Activity.playing("메시지 기다리는 중!"))
-            .setMaxReconnectDelay(32)
-            .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_VOICE_STATES) // 필요한 인텐트 모두 활성화
-            .addEventListeners(
-                context.getBean(StudyBotDiscordListener.class),
-                context.getBean(VoiceChannelTracker.class)
-            )
-            .build();
+    public static void main(String[] args) {
+        SpringApplication.run(StudybotApplication.class, args);
     }
 }
